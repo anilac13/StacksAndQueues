b03b1ea7ee63580426a98bd4ed85d00e36c457e1
anilac13 <anil4u2496@gmail.com>
2023-05-19 13:51:36 +0530

    [Anil] Add:Create queue with elements 56,30,70

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -6,10 +6,11 @@ namespace StacksAndQueues
         static void Main(string[] args)
         {
             Stack stack = new Stack();
+            Queue queue = new Queue();
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nChoose option to perform \n1.Add elements to Stack \n2.Display \n3.Peek and Pop elements \n4.Exit");
+                Console.WriteLine("\nChoose option to perform \n1.Add elements to Stack \n2.Display \n3.Peek and Pop elements \n4.Add elements to Queue \n5.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -19,13 +20,19 @@ namespace StacksAndQueues
                         stack.Push(70);
                         break;
                     case 2:
-                        stack.Display();
+                        //stack.Display();
+                        queue.Display();
                         break;
                     case 3:
                         stack.Peek();
                         stack.Pop();
                         break;
                     case 4:
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        break;
+                    case 5:
                         flag = false;
                         break;
                 }
---
+++ StacksAndQueues/Queue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class Queue
+    {
+        private LinkedList queue = new LinkedList();
+        public void Enqueue(int data)
+        {
+            queue.AddToLast(data);
+        }
+        public void Display()
+        {
+            queue.Display();
+        }
+    }
+}
